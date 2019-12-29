2b5b34f463d0024d22be65713740103daace2512
Dwi Heryanto <m.dwiheryanto2@gmail.com>
2019-12-29 16:47:58 +0700

    add url herkouapp to origins

--- src/main/java/com/in28minutes/fullstack/springboot/maven/crud/springbootfullstackcrudfullstackwithmaven/course/courseResource.java
+++ src/main/java/com/in28minutes/fullstack/springboot/maven/crud/springbootfullstackcrudfullstackwithmaven/course/courseResource.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-@CrossOrigin(origins = {"http://localhost:3001", "http://localhost:4200"})
+@CrossOrigin(origins = {"http://localhost:3001", "http://localhost:4200", "https://heroku-spring-boot-crud.herokuapp.com/"})
 @RestController
 public class courseResource {
 	@Autowired
